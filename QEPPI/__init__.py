277bd6b148c5b8a9d68801ff3e3bf67d778acbfa
Jianmin Wang <drugai@hotmail.com>
2025-06-09 10:28:55 +0900

    Update __init__.py

--- QEPPI/__init__.py
+++ QEPPI/__init__.py
@@ -1,3 +1,8 @@
+"""Public package interface for QEPPI-community.
+
+Based on the original implementation from `ohuelab/QEPPI`.
+"""
+
 from .version import __version__
 from .constant import *
 from .util import *
