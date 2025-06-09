8af7e556972a001050157f47346e905ada90f538
Jianmin Wang <drugai@hotmail.com>
2025-06-09 10:31:18 +0900

    Update version.py

--- QEPPI/version.py
+++ QEPPI/version.py
@@ -1,1 +1,7 @@
-__version__ = "0.1.11"
+"""Version information for the QEPPI-community package.
+
+This file originates from the `ohuelab/QEPPI` project and stores the package
+version string used across the codebase.
+"""
+
+__version__ = "0.1.13"
